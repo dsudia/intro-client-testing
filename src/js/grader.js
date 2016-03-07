648e206a6cf1d72b6e40d26d80f9b3e5e8406f24
David Sudia <dsudia@gmail.com>
2016-03-07 09:36:07 -0700

    write test for letterGrader

---
+++ src/js/grader.js
@@ -0,0 +1,5 @@
+// function letterGrader (score) {
+//   if (score <= 100 && score >= 90) {
+//     return 'A';
+//   }
+// }
--- src/test/test.js
+++ src/test/test.js
@@ -3,7 +3,19 @@ var expect = chai.expect;
 
 // sample!
 describe('Compare Numbers', function() {
-  it('1 should equal 1', function() {
-    expect(1).to.equal(1);
+  it('should convert a grade between 90 and 100 inclusive to an A', function() {
+    expect(letterGrader(95)).to.equal('A');
+  });
+  it('should convert a grade between 80 and 89 inclusive to a B', function() {
+    expect(letterGrader(85)).to.equal('B');
+  });
+  it('should convert a grade between 70 and 79 inclusive to a C', function() {
+    expect(letterGrader(85)).to.equal('C');
+  });
+  it('should convert a grade between 60 and 69 inclusive to a D', function() {
+    expect(letterGrader(85)).to.equal('B');
+  });
+  it('should convert a grade between 0 and 59 inclusive to an F', function() {
+    expect(letterGrader(85)).to.equal('F');
   });
 });
